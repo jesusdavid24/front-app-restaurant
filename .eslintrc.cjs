30f21c1a9fc00591f06af7318c74da29fc9f3cd0
jesus bravo <jd_bravo24@hotmail.com>
2023-08-13 20:26:04 -0500

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,25 @@ module.exports = {
   env: {
     es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+  extends: 'airbnb',
+  'react/jsx-runtime': 'plugin:react/jsx-runtime',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   rules: {
     'react/function-component-definition': [
       'error', {
