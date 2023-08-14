4f066d75374b969dcf9682e09b541a616af39f47
jesus bravo <jd_bravo24@hotmail.com>
2023-08-13 22:23:18 -0500

    deploy

--- src/components/restaurant-slider/RestaurantSlider.jsx
+++ src/components/restaurant-slider/RestaurantSlider.jsx
@@ -7,7 +7,7 @@ const RestaurantSlider = () => {
     <div className="restaurant-slider">
       <div className="restaurant-slider__box">
         <img
-          src="../src/assets/img/restuarant-italy.jpg"
+          src="../src/assets/img/restuarant-list.png"
           className="restaurant-slider__box__img"
         />
         <h2 className="restaurant-slider__box__name">Italian Restro</h2>
