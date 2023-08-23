2c10bafd9a538a3a94a2edc0b9730738bd689f32
jesus bravo <jd_bravo24@hotmail.com>
2023-08-23 10:01:53 -0500

    img footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,6 +3,7 @@ import { Accordion } from '@mantine/core';
 import MapBox from '../MapBox';
 import FooterWide from '../FooterWide';
 import './index.scss';
+
 const Footer = () => {
   const coordinates = [-73.9999, 40.6892];
   return (
@@ -22,7 +23,7 @@ const Footer = () => {
                 <div className='footer__panel__contact__logo-container'>
                   <img
                     id='logo'
-                    src='/src/assets/icons/logo2.png'
+                    src='/icons/logo2.png'
                     className='footer__panel__contact__logo'
                   />
                   <h1 id='logo' className='footer__panel__contact__title'>
@@ -37,7 +38,7 @@ const Footer = () => {
                 <h2 className='footer__panel__contact__data'>
                   <img
                     id='logo'
-                    src='/src/assets/icons/position.png'
+                    src='/icons/position.png'
                     className='footer__panel__contact__icon'
                   />
                   &nbsp; A-32, Albany, New York.
@@ -45,7 +46,7 @@ const Footer = () => {
                 <h2 className='footer__panel__contact__data'>
                   <img
                     id='logo'
-                    src='/src/assets/icons/telephone.png'
+                    src='/icons/telephone.png'
                     className='footer__panel__contact__icon'
                   />
                   &nbsp; 518 - 457 - 5181
@@ -53,7 +54,7 @@ const Footer = () => {
                 <h2 className='footer__panel__contact__data'>
                   <img
                     id='logo'
-                    src='/src/assets/icons/mail.png'
+                    src='/icons/mail.png'
                     className='cfooter__panel__contact__icon'
                   />
                   &nbsp; Contact@Gmail.com
@@ -128,7 +129,7 @@ const Footer = () => {
             <Accordion.Panel className='footer__panel'>
               <div className='footer__panel__topics'>
                 <img
-                  src='../src/assets/img/taxi.jpg'
+                  src='../img/taxi.jpg'
                   className='footer__panel__topics__image'
                 />
                 <div className='footer__panel__topics__cotent-box'>
@@ -143,7 +144,7 @@ const Footer = () => {
             <Accordion.Panel className='footer__panel'>
               <div className='footer__panel__topics'>
                 <img
-                  src='../src/assets/img/taxi2.jpg'
+                  src='../img/taxi2.jpg'
                   className='footer__panel__topics__image2'
                 />
                 <div className='footer__panel__topics__cotent-box'>
@@ -164,26 +165,26 @@ const Footer = () => {
       <div className='footer__networks'>
         <div className='footer__networks__buttons'>
           <img
-            src='../src/assets/icons/facebook.png'
+            src='../icons/facebook.png'
             className='footer__networks__icon'
           />
           <img
-            src='../src/assets/icons/instagram.png'
+            src='../icons/instagram.png'
             className='footer__networks__icon'
           />
           <img
-            src='../src/assets/icons/twitter.png'
+            src='../icons/twitter.png'
             className='footer__networks__icon'
           />
           <img
-            src='../src/assets/icons/google.png'
+            src='../icons/google.png'
             className='footer__networks__icon'
           />
         </div>
         <h2 className='footer__copyright'>
           Copyright 2023 Rica By
           <img
-            src='../src/assets/icons/hearth.png'
+            src='../icons/hearth.png'
             className='footer__copyright__hearth'
           />
           Pixelstrap
