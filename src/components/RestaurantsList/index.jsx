4e722ab3536e077652fd79eb1e6ac7a2e969f58c
jesus bravo <jd_bravo24@hotmail.com>
2023-08-22 20:26:19 -0500

    rename restaurant card

--- src/components/RestaurantsList/index.jsx
+++ src/components/RestaurantsList/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Restaurantcard from "../RestaurantCard";
+import Restaurantcard from "../CardsRestaurants";
 import { restaurants } from "../../assets/data/restaurants";
 import "./index.scss";
 
