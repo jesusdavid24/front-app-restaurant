c7e6c6df50a9f22dd1f5b59d1c94c2995fca7e46
Rodrigo <rhbon.08@gmail.com>
2023-08-25 21:13:36 -0500

    Responsive page up to 992px, styles were adjusted

--- src/components/PaymentItems/index.jsx
+++ src/components/PaymentItems/index.jsx
@@ -1,177 +1,267 @@
-import React, { useState } from "react"
-import { Accordion, useMantineTheme, rem } from '@mantine/core';
-import './index.scss';
+import React, { useState } from "react";
+import { Accordion, useMantineTheme, rem } from "@mantine/core";
+import "./index.scss";
 
 const Paymentitems = ({ paymentitems, addPaymentitem, removePaymentitem }) => {
   const handleAddpaymentitem = () => {
-    const item = prompt('Ingrese un nuevo producto: ')
+    const item = prompt("Ingrese un nuevo producto: ");
     if (item) {
-      addPaymentitem(item)
+      addPaymentitem(item);
     }
-  }
+  };
   const handleRemoveAddress = (index) => {
-    if (window.confirm('¿Are you sure you want to remove?')) {
-      removePaymentitem(index)
+    if (window.confirm("¿Are you sure you want to remove?")) {
+      removePaymentitem(index);
     }
-  }
+  };
 
-  const [selectedOption, setSelectOption] = useState (null);
+  const [selectedOption, setSelectOption] = useState(null);
 
   return (
-      <div className="payment-items">
-        <Accordion variant="contained"
-          chevron={false}
-          className="payment-items__accordion"
+    <div className="payment-items">
+      <Accordion
+        variant="contained"
+        chevron={false}
+        className="payment-items__accordion"
+      >
+        <Accordion.Item
+          className="payment-items__accordion__item"
+          value="photos"
         >
-          <Accordion.Item className="payment-items__accordion__item" value="photos">
-            <Accordion.Control>
-              <input type="radio" name="debit_card" className="radio_debit_Card" checked={selectedOption === "td"} onChange={()=> setSelectOption("td")} />
-              <label className="space" htmlFor="debit_card">Debit Card</label>
-            </Accordion.Control>
-            <Accordion.Panel>
-              <div className="container">
-                <div className="container_nameCard">
-                  <label htmlFor="nameCard">Name On Card</label>
-                  <input type="text" name="nameCard" />
-                  <label htmlFor="cardNumber">Card Number</label>
-                  <div className="container_cardNumber">
-                    <input type="text" name="cardNumber" />
-                    <img className="container_img_tc_debit" src="/img/creditcards.png" alt="" />
-                  </div>
+          <Accordion.Control>
+            <input
+              type="radio"
+              name="debit_card"
+              className="radio_debit_Card"
+              checked={selectedOption === "td"}
+              onChange={() => setSelectOption("td")}
+            />
+            <label className="space" htmlFor="debit_card">
+              Debit Card
+            </label>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <div className="container">
+              <div className="container_nameCard">
+                <label htmlFor="nameCard">Name On Card</label>
+                <input type="text" name="nameCard" />
+                <label htmlFor="cardNumber">Card Number</label>
+                <div className="container_cardNumber">
+                  <input type="text" name="cardNumber" />
+                  <img
+                    className="container_img_tc_debit"
+                    src="/img/creditcards.png"
+                    alt=""
+                  />
                 </div>
-                <br />
-                <div className="container_dates">
+              </div>
+              <br />
+              <div className="container-dates">
+                <div className="container-dates__confirm">
                   <label htmlFor="month_label">Month</label>
                   <input type="text" name="month_label" />
+                </div>
+                <div className="container-dates__confirm">
                   <label htmlFor="year_label">Year</label>
                   <input type="text" name="year_label" />
+                </div>
+                <div className="container-dates__confirm">
                   <label htmlFor="pay_cvv">Cvv</label>
                   <div className="container_cardNumber">
                     <input type="text" name="pay_cvv" />
-                    <img className="container_img_tc_debit" src="/img/cvv.png" alt="" />
+                    <img
+                      className="container_img_tc_debit"
+                      src="/img/cvv.png"
+                      alt=""
+                    />
                   </div>
-                </div>
-                <br />
-                <div className="container_buttonP">
-                  <button>MAKE PAYMENT</button>
                 </div>
               </div>
-            </Accordion.Panel>
-          </Accordion.Item>
-          <Accordion.Item className="payment-items__accordion__item" value="print">
-            <Accordion.Control>
-              <input type="radio" name="credit_card" className="radio_debit_Card" checked={selectedOption === "tc"} onChange={()=> setSelectOption("tc")}/>
-              <label className="space" htmlFor="credit_card">Credit Card</label>
-            </Accordion.Control>
-            <Accordion.Panel>
-              <div className="container">
-                <div className="container_nameCard">
-                  <label htmlFor="nameCard">Name On Card</label>
-                  <input type="text" name="nameCard" />
-                  <label htmlFor="cardNumber">Card Number</label>
-                  <div className="container_cardNumber">
-                    <input type="text" name="cardNumber" />
-                    <img className="container_img_tc" src="/img/creditcards.png" alt="" />
-                  </div>
+              <br />
+              <div className="container_buttonP">
+                <button>MAKE PAYMENT</button>
+              </div>
+            </div>
+          </Accordion.Panel>
+        </Accordion.Item>
+        <Accordion.Item
+          className="payment-items__accordion__item"
+          value="print"
+        >
+          <Accordion.Control>
+            <input
+              type="radio"
+              name="credit_card"
+              className="radio_debit_Card"
+              checked={selectedOption === "tc"}
+              onChange={() => setSelectOption("tc")}
+            />
+            <label className="space" htmlFor="credit_card">
+              Credit Card
+            </label>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <div className="container">
+              <div className="container_nameCard">
+                <label htmlFor="nameCard">Name On Card</label>
+                <input type="text" name="nameCard" />
+                <label htmlFor="cardNumber">Card Number</label>
+                <div className="container_cardNumber">
+                  <input type="text" name="cardNumber" />
+                  <img
+                    className="container_img_tc"
+                    src="/img/creditcards.png"
+                    alt=""
+                  />
                 </div>
-                <br />
-                <div className="container_dates">
+              </div>
+              <br />
+              <div className="container-dates">
+                <div className="container-dates__confirm">
                   <label htmlFor="month_label">Month</label>
-                  <input type="text" name="month_label" placeholder="Month..." />
+                  <input
+                    type="text"
+                    name="month_label"
+                    placeholder="Month..."
+                  />
+                </div>
+                <div className="container-dates__confirm">
                   <label htmlFor="year_label">Year</label>
                   <input type="text" name="year_label" placeholder="Year..." />
+                </div>
+                <div className="container-dates__confirm">
                   <label htmlFor="pay_cvv">Cvv</label>
                   <div className="container_cardNumber">
                     <input type="text" name="pay_cvv" />
-                    <img className="container_img_tc" src="/img/cvv.png" alt="" />
+                    <img
+                      className="container_img_tc"
+                      src="/img/cvv.png"
+                      alt=""
+                    />
                   </div>
                 </div>
               </div>
-            </Accordion.Panel>
-          </Accordion.Item>
-          <Accordion.Item className="payment-items__accordion__item" value="camera">
-            <Accordion.Control>
-              <input type="radio" name="net_banking" className="radio_debit_Card" checked={selectedOption === "nb"} onChange={()=> setSelectOption("nb")}/>
-              <label className="space" htmlFor="net_banking">Net Banking</label>
-            </Accordion.Control>
-            <Accordion.Panel>
-              <div>
-                <div className="container_banks">
-                  <div className="check">
-                    <input type="radio" name="industrial_bank" />
-                    <label className="space" htmlFor="industrial_bank">Industrial & Commercial Bank</label>
-                  </div>
-                  <div className="check">
-                    <input type="radio" name="agricultura_bank" />
-                    <label className="space" htmlFor="agricultura_bank">Agricultural Bank</label>
-                  </div>
-                  <div className="check">
-                    <input type="radio" name="bank_america" />
-                    <label className="space" htmlFor="bank_america">Bank of America</label>
-                  </div>
-                  <div className="check">
-                    <input type="radio" name="construction_bank" />
-                    <label className="space" htmlFor="construction_bank">Construction Bank Corp.</label>
-                  </div>
-                  <div className="check">
-                    <input type="radio" name="hsbc_bank" />
-                    <label className="space" htmlFor="hsbc_bank">HSBC Holdings</label>
-                  </div>
-                  <div className="check">
-                    <input type="radio" name="morgan_bank" />
-                    <label className="space" htmlFor="morgan_bank">JPMorgan Chase & Co</label>
-                  </div>
+            </div>
+          </Accordion.Panel>
+        </Accordion.Item>
+        <Accordion.Item
+          className="payment-items__accordion__item"
+          value="camera"
+        >
+          <Accordion.Control>
+            <input
+              type="radio"
+              name="net_banking"
+              className="radio_debit_Card"
+              checked={selectedOption === "nb"}
+              onChange={() => setSelectOption("nb")}
+            />
+            <label className="space" htmlFor="net_banking">
+              Net Banking
+            </label>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <div>
+              <div className="container_banks">
+                <div className="check">
+                  <input type="radio" name="industrial_bank" />
+                  <label className="space" htmlFor="industrial_bank">
+                    Industrial & Commercial Bank
+                  </label>
                 </div>
-                <br />
-                <div className="container_bank">
-                  <label htmlFor="select_bank">Select Bank</label>
-                  <input type="text" name="select_bank" placeholder="Choose Bank..." />
+                <div className="check">
+                  <input type="radio" name="agricultura_bank" />
+                  <label className="space" htmlFor="agricultura_bank">
+                    Agricultural Bank
+                  </label>
                 </div>
-              </div>
-            </Accordion.Panel>
-          </Accordion.Item>
-          <Accordion.Item className="payment-items__accordion__item" value="wall">
-            <Accordion.Control>
-              <input type="radio" name="my_wallet" className="radio_debit_Card" checked={selectedOption === "mw"} onChange={()=> setSelectOption("mw")}/>
-              <label className="space" htmlFor="my_wallet">My Wallet</label>
-            </Accordion.Control>
-            <Accordion.Panel>
-              <div>
-                <div className="container_banks">
-                  <div className="check">
-                    <input type="radio" name="industrial_bank" />
-                    <label htmlFor="industrial_bank">Adyen</label>
-                  </div>
-                  <div className="check">
-                    <input type="radio" name="agricultura_bank" />
-                    <label htmlFor="agricultura_bank">AlliedWallet</label>
-                  </div>
-                  <div className="check">
-                    <input type="radio" name="bank_america" />
-                    <label htmlFor="bank_america">Brinks</label>
-                  </div>
-                  <div className="check">
-                    <input type="radio" name="construction_bank" />
-                    <label htmlFor="construction_bank">Airtel Money</label>
-                  </div>
-                  <div className="check">
-                    <input type="radio" name="hsbc_bank" />
-                    <label htmlFor="hsbc_bank">Apple Pay</label>
-                  </div>
-                  <div className="check">
-                    <input type="radio" name="morgan_bank" />
-                    <label htmlFor="morgan_bank">CardFree</label>
-                  </div>
+                <div className="check">
+                  <input type="radio" name="bank_america" />
+                  <label className="space" htmlFor="bank_america">
+                    Bank of America
+                  </label>
                 </div>
-                <br />
-                <div className="container_payments">
-                  <button>MAKE PAYMENT</button>
+                <div className="check">
+                  <input type="radio" name="construction_bank" />
+                  <label className="space" htmlFor="construction_bank">
+                    Construction Bank Corp.
+                  </label>
+                </div>
+                <div className="check">
+                  <input type="radio" name="hsbc_bank" />
+                  <label className="space" htmlFor="hsbc_bank">
+                    HSBC Holdings
+                  </label>
+                </div>
+                <div className="check">
+                  <input type="radio" name="morgan_bank" />
+                  <label className="space" htmlFor="morgan_bank">
+                    JPMorgan Chase & Co
+                  </label>
                 </div>
               </div>
-            </Accordion.Panel>
-          </Accordion.Item>
-        </Accordion>
-      </div>
-  )
-}
-export default Paymentitems
+              <br />
+              <div className="container_bank">
+                <label htmlFor="select_bank">Select Bank</label>
+                <input
+                  type="text"
+                  name="select_bank"
+                  placeholder="Choose Bank..."
+                />
+              </div>
+            </div>
+          </Accordion.Panel>
+        </Accordion.Item>
+        <Accordion.Item className="payment-items__accordion__item" value="wall">
+          <Accordion.Control>
+            <input
+              type="radio"
+              name="my_wallet"
+              className="radio_debit_Card"
+              checked={selectedOption === "mw"}
+              onChange={() => setSelectOption("mw")}
+            />
+            <label className="space" htmlFor="my_wallet">
+              My Wallet
+            </label>
+          </Accordion.Control>
+          <Accordion.Panel>
+            <div>
+              <div className="container_banks">
+                <div className="check">
+                  <input type="radio" name="industrial_bank" />
+                  <label htmlFor="industrial_bank">Adyen</label>
+                </div>
+                <div className="check">
+                  <input type="radio" name="agricultura_bank" />
+                  <label htmlFor="agricultura_bank">AlliedWallet</label>
+                </div>
+                <div className="check">
+                  <input type="radio" name="bank_america" />
+                  <label htmlFor="bank_america">Brinks</label>
+                </div>
+                <div className="check">
+                  <input type="radio" name="construction_bank" />
+                  <label htmlFor="construction_bank">Airtel Money</label>
+                </div>
+                <div className="check">
+                  <input type="radio" name="hsbc_bank" />
+                  <label htmlFor="hsbc_bank">Apple Pay</label>
+                </div>
+                <div className="check">
+                  <input type="radio" name="morgan_bank" />
+                  <label htmlFor="morgan_bank">CardFree</label>
+                </div>
+              </div>
+              <br />
+              <div className="container_payments">
+                <button>MAKE PAYMENT</button>
+              </div>
+            </div>
+          </Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  );
+};
+export default Paymentitems;
--- src/pages/Summary/index.jsx
+++ src/pages/Summary/index.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import AddressList from "../../components/AddressList";
-import Paymentitems from "../../components/PaymentItems"
-import CardView from "../../components/CardView"
-import AlwaysFirst from "../../components/AlwaysFirst"
+import Paymentitems from "../../components/PaymentItems";
+import CardView from "../../components/CardView";
+import AlwaysFirst from "../../components/AlwaysFirst";
 import "./index.scss";
-import Cart from "../../components/Cart"
+import Cart from "../../components/Cart";
 
 const Checkout = () => {
   const [paymentitems, setPaymentitems] = useState([]);
@@ -21,29 +21,35 @@ const Checkout = () => {
 
   return (
     <div className="summary">
-      <div className="container_immg">
-        <img className="object" src="/img/1.jpg" />
+      <div className="summary__container-immg">
+        <img className="summary__object" src="/img/1.jpg" />
       </div>
-      <div className="checkout_container">
-        <div className="cart_container">
-          <Cart />
-        </div>
-        <div className="card_always">
-          <AlwaysFirst />
-        </div>
-        <div className="container_card_address">
-          <AddressList />
-          <div className="card_view">
-            <CardView />
+      <div className="summary__checkout-container">
+        <div className="summary__contentca">
+          <div className="summary__cartfirst">
+            <div className="summary__cart-container">
+              <Cart />
+            </div>
+            <div className="summary__card-always">
+              <AlwaysFirst />
+            </div>
           </div>
         </div>
-        <div className="summary__payment">
-          <h2 className="summary__payment__title">Payment:</h2>
-          <Paymentitems
-            paymentitems={paymentitems}
-            addPaymentitem={addPaymentitem}
-            removePaymentitem={removePaymentitem}
+        <div className="summary__delypay">
+          <div className="summary__card-address">
+            <AddressList />
+            <div className="summary__card-view">
+              <CardView />
+            </div>
+          </div>
+          <div className="summary__payment">
+            <h2 className="summary__payment__title">Payment:</h2>
+            <Paymentitems
+              paymentitems={paymentitems}
+              addPaymentitem={addPaymentitem}
+              removePaymentitem={removePaymentitem}
             />
+          </div>
         </div>
       </div>
     </div>
