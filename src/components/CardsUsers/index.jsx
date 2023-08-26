f7bbbe101f6506178031bdb88d771bb02f3a7e04
jesus bravo <jd_bravo24@hotmail.com>
2023-08-25 23:09:59 -0500

    add card

---
+++ src/components/CardsUsers/index.jsx
@@ -0,0 +1,19 @@
+
+const cardsUsers = () => {
+  return (
+    <div className="card">
+      <div className="card__container">
+        <p class="card__container__data__">
+          Popular this month
+        </p>
+        <p>
+          Powered By
+        </p>
+        <p>Uiverse
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default cardsUsers
---
+++ src/components/DashboardAdmin/index.jsx
@@ -0,0 +1,26 @@
+
+
+const dashboardAdmin = () => {
+  return (
+    <div className="admin">
+      <div className="admin__container">
+        <div className="admin__container__data">
+          <div className="admin__container__data__users">
+
+          </div>
+          <div className="admin__container__data__admin">
+
+          </div>
+          <div className="admin__container__data__restaurant">
+
+          </div>
+        </div>
+        <div className="admin__container__users">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default dashboardAdmin
