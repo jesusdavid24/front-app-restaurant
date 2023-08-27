62a882edd6523e03e4339e920df9bc030492b829
Cristian <cristian.jimenezsa@hotmail.com>
2023-08-27 00:11:28 -0500

    reviews fetch

--- src/components/Reviews/index.jsx
+++ src/components/Reviews/index.jsx
@@ -1,36 +1,37 @@
-import React from "react";
-import { reviews } from "../../assets/data/reviews";
-import "./index.scss";
+// import { reviews } from '../../assets/data/reviews';
+import './index.scss';
 
-const Reviews = () => {
+const Reviews = ({ restaurant }) => {
+  const { reviews } = restaurant;
+  console.log(restaurant);
   return (
-    <div className="restaurant-reviews">
+    <div className='restaurant-reviews'>
       {reviews.map((review, index) => (
-        <div key={index} className="restaurant-reviews__review">
-          <div className="restaurant-reviews__review__rating">
-            <span className="restaurant-reviews__review__star">
+        <div key={index} className='restaurant-reviews__review'>
+          <div className='restaurant-reviews__review__rating'>
+            <span className='restaurant-reviews__review__star'>
               {[1, 2, 3, 4, 5].map((item) =>
                 item <= review.rating ? (
                   <img
                     key={item}
-                    src="/icons/star-fill.png"
-                    style={{ width: "15px" }}
+                    src='/icons/star-fill.png'
+                    style={{ width: '15px' }}
                   />
                 ) : (
                   <img
                     key={item}
-                    src="/icons/star.png"
-                    style={{ width: "15px" }}
+                    src='/icons/star.png'
+                    style={{ width: '15px' }}
                   />
                 )
               )}
             </span>
             <h6>{review.title}</h6>
           </div>
-          <h6 className="restaurant-reviews__review__author">
+          <h6 className='restaurant-reviews__review__author'>
             By {review.user}, {review.date}
           </h6>
-          <p className="restaurant-reviews__review__content">
+          <p className='restaurant-reviews__review__content'>
             {review.content}
           </p>
         </div>
