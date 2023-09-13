e0a21140a9bed0d01cfc163d830fd2280f319d26
Jesus Bravo <jd_bravo24@hotmail.com>
2023-09-13 14:36:38 -0500

    merge from dev to main (#57)
    * pull origin main
    
    * review

--- src/api/login.js
+++ src/api/login.js
@@ -5,12 +5,12 @@ const URL = `${import.meta.env.VITE_BASE_URL}`;
 export const login = async (form) => {
   try {
     const { data } = await axios.post(`${URL}/auth/local/login`, form)
-    return data
+    return data;
   } catch(error) {
     console.log(error)
     return error.message
   };
-}
+};
 
 
 
