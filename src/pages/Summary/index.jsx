b8bf60ed478ea439490884677a9e5bce2d7764bb
Rodrigo <rhbon.08@gmail.com>
2023-09-14 00:09:04 -0500

    Stripe installation, validation of debit card number and styles

--- src/pages/Summary/index.jsx
+++ src/pages/Summary/index.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { clearCart } from '../../store/redux/slices/cartSlice';
-import AddressList from '../../components/AddressList';
-import Paymentitems from '../../components/PaymentItems';
-import CardView from '../../components/CardView';
-import AlwaysFirst from '../../components/AlwaysFirst';
-import Cart from '../../components/Cart';
-import './index.scss';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { clearCart } from "../../store/redux/slices/cartSlice";
+import AddressList from "../../components/AddressList";
+import Paymentitems from "../../components/PaymentItems";
+import CardView from "../../components/CardView";
+import AlwaysFirst from "../../components/AlwaysFirst";
+import Cart from "../../components/Cart";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import "./index.scss";
+
+const stripePromise = loadStripe(
+  "pk_test_51NiN00Isblv2sRW8edxjWByXYrbDZG86nyVa4KDjJCMLZi5QaOjiqlOb02jafd0jRDWI3OtrQmr3XCS94ILcFDLJ00y4dMEfMb"
+);
 
 const Checkout = () => {
   const navigate = useNavigate();
@@ -31,43 +37,46 @@ const Checkout = () => {
   };
 
   return (
-    <div className='summary'>
-      <div className='summary__container-immg'>
-        <img className='summary__object' src='/img/1.jpg' />
+    <div className="summary">
+      <div className="summary__container-immg">
+        <img className="summary__object" src="/img/1.jpg" />
       </div>
-      <div className='summary__checkout-container'>
-        <div className='summary__contentca'>
-          <div className='summary__cartfirst'>
-            <div className='summary__cart-container'>
+      <div className="summary__checkout-container">
+        <div className="summary__contentca">
+          <div className="summary__cartfirst">
+            <div className="summary__cart-container">
               <Cart>
-                <div className='order-cart__fill__bottom__buttons-box'>
+                <div className="order-cart__fill__bottom__buttons-box">
                   <button
-                    className='order-cart__fill__bottom__buttons-box__button'
-                    onClick={handleClear}>
+                    className="order-cart__fill__bottom__buttons-box__button"
+                    onClick={handleClear}
+                  >
                     Clear
                   </button>
                 </div>
               </Cart>
             </div>
-            <div className='summary__card-always'>
+            <div className="summary__card-always">
               <AlwaysFirst />
             </div>
           </div>
         </div>
-        <div className='summary__delypay'>
-          <div className='summary__card-address'>
+        <div className="summary__delypay">
+          <div className="summary__card-address">
             <AddressList />
-            <div className='summary__card-view'>
+            <div className="summary__card-view">
               <CardView />
             </div>
           </div>
-          <div className='summary__payment'>
-            <h2 className='summary__payment__title'>Payment:</h2>
-            <Paymentitems
-              paymentitems={paymentitems}
-              addPaymentitem={addPaymentitem}
-              removePaymentitem={removePaymentitem}
-            />
+          <div className="summary__payment">
+            <h2 className="summary__payment__title">Payment:</h2>
+            <Elements stripe={stripePromise}>
+              <Paymentitems
+                paymentitems={paymentitems}
+                addPaymentitem={addPaymentitem}
+                removePaymentitem={removePaymentitem}
+              />
+            </Elements>
           </div>
         </div>
       </div>
